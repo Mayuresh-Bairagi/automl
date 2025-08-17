6d7cab8579ea297a0b8b1cf01d1812ca8d8ac259
Mayuresh Bairagi <mayureshbairagi304@gmail.com>
2025-08-17 12:55:49 +0530

    Refactor project structure: update package names in setup.py, simplify requirements.txt.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="document_portal",
+    name="auto_ml_model",
     author="Mayuresh Bairagi",
     version="0.1",
     packages=find_packages(),
